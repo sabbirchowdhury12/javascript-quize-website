7ebc3198522bbc2ff9eebf77a41c3089d59165b6
sabbir <sabbir@gmail.com>
2023-09-18 20:23:33 +0600

    question style done

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const startBtn = document.querySelector('.start-btn');
-const popupInfo = document.querySelector('.popup-info');
-const exitBtn = document.querySelector('.exit-btn');
-const main = document.querySelector('.main');
-const continueBtn = document.querySelector('.continue-btn');
-const quizSection = document.querySelector('.quiz-section');
-const quizBox = document.querySelector('.quiz-box');
-const resultBox = document.querySelector('.result-box');
-const tryAgainBtn = document.querySelector('.tryAgain-btn');
-const goHomeBtn = document.querySelector('.goHome-btn');
+const popupInfo = document.querySelector(".popup-info");
+const main = document.querySelector(".main");
+const quizSection = document.querySelector(".quiz-section");
+const nextBtn = document.querySelector(".next-btn");
+const questionText = document.querySelector(".question-text");
+
+function startQuiz() {
+  popupInfo.classList.add("active");
+  main.classList.add("active");
+}
+function exitQuiz() {
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+}
+
+//continue buton
+function continueQuiz() {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+  showQuestion(0);
+}
+
+let questionCount = 0;
+
+nextBtn.onclick = () => {
+  questionCount++;
+  showQuestion(questionCount);
+};
+
+function showQuestion(index) {
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+}
